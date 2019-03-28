b984cfd1f69d0d4e1c9edff854fb69daeecac777
Lucas Henrique <lucas.coelho@oi.net.br>
2019-03-28 18:40:44 -0300

    New function for find registers

--- app/db/hand.go
+++ app/db/hand.go
@@ -1,21 +1,24 @@
 package db
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 )
 
 // GetAll return all register database
-func GetAll(db *gorm.DB) (*OS, error) {
-	OS := OS{}
+func GetAll(db *gorm.DB) (*[]OS, error) {
+	OS := []OS{}
 	if result := db.First(&OS); result.Error != nil {
 		return nil, result.Error
 	}
 	return &OS, nil
 }
 
-// Get Find register
-func Get(db *gorm.DB, nome string) (*OS, error) {
-	OS := OS{}
+// GetNome Find register
+func GetNome(db *gorm.DB, nome string) (*[]OS, error) {
+	OS := []OS{}
 	db.Where("nomeCliente = ?", nome).Find(&OS)
 	if db.Error != nil {
 		return nil, db.Error
@@ -29,4 +32,19 @@ func DeleteID(db *gorm.DB, id int) error {
 		return db.Error
 	}
 	return nil
+}
+
+// Get ...
+func Get(db *gorm.DB, f map[string]interface{}) (*[]OS, error) {
+	OS := []OS{}
+	if len(f) == 0 {
+		return nil, errors.New("Parameter is empty")
+	}
+	query := db.New()
+	for col, val := range f {
+		query.Where(fmt.Sprintf("%s = ?", col), val)
+	}
+	query.Find(&OS)
+
+	return &OS, nil
 }
